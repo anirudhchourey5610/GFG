8393b92c2e73377ad43c781e4eba582284867fbb
Anirudh12 <uchourey16@gmail.com>
2025-09-11 00:06:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int x) {
+        // code here
+        int n=arr.size();
+         int ansIndex=-1;
+        int ansIndex1=-1;
+        int s=0;
+      int e=n-1;
+      int mid=s+(e-s)/2;
+      while(s<=e){
+          if(arr[mid]==x){
+              ansIndex=mid;
+              e=mid-1;
+          }
+          else if(arr[mid]>x){
+              e=mid-1;
+          }
+          else if(arr[mid]<x){
+              s=mid+1;
+          }
+          mid=s+(e-s)/2;
+      }
+      if(ansIndex==-1) ansIndex=0;
+      int s1=0;
+      int e1=n-1;
+      int mid1=s1+(e1-s1)/2;
+      while(s1<=e1){
+          if(arr[mid1]==x){
+              ansIndex1=mid1;
+              s1=mid1+1;
+          }
+          else if(arr[mid1]>x){
+              e1=mid1-1;
+          }
+          else if(arr[mid1]<x){
+              s1=mid1+1;
+          }
+          mid1=s1+(e1-s1)/2;
+      }
+      return ansIndex1-ansIndex+1;
+    }
+};
