3400649f7953e7706557d687a71f8c48fdce622c
Anirudh12 <uchourey16@gmail.com>
2026-03-06 09:13:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.cpp
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -2,11 +2,14 @@ class Solution {
   public:
     vector<int> findDuplicates(vector<int>& arr) {
         // code here
+        unordered_map<int,int> freq;
         vector<int> ans;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size()-1;i++){
-            if(arr[i]==arr[i+1]){
-                ans.push_back(arr[i]);
+        for(int i=0;i<arr.size();i++){
+            freq[arr[i]]++;
+        }
+        for(auto it:freq){
+            if(it.second>1){
+                ans.push_back(it.first);
             }
         }
         return ans;
