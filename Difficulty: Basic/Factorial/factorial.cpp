4460f20cb37cbc956233b4ac2029f35d90b56c8f
Anirudh12 <uchourey16@gmail.com>
2026-03-09 17:29:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.cpp
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -1,14 +1,10 @@
 class Solution {
   public:
-  int fact(int n){
-      if(n==0 || n==1){
-          return 1;
-      }
-      return n*fact(n-1);
-  }
-    void factorial(int n) {
+    int factorial(int n) {
         // code here
-        int ansFinal=fact(n);
-        cout<<ansFinal<<endl;
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*factorial(n-1);
     }
 };
